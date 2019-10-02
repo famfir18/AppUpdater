bbd269231071823e0c02b45a7fc6284824a357a0
Faisal Amirul Firdaus <faisal.firdaus@fusi24.com>
2019-10-02 16:24:18 +0700

    first commit

---
+++ app/src/main/java/com/famfir/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.famfir.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AppUpdater(this)
+                .setUpdateFrom(UpdateFrom.GOOGLE_PLAY)
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setDisplay(Display.DIALOG)
+                .setTitleOnUpdateAvailable("Update available")
+                .setContentOnUpdateAvailable("Update aplikasi untuk menggunakan aplikasi")
+                .setButtonUpdate("Update")
+                .setCancelable(false)
+                .start();
+    }
+}
